dedd4cd9031926c3a9a190712c02e498114dc7df
dalepotter <dalepotter@gmail.com>
2018-03-14 17:06:52 +0000

    Add IP of dev server to settings

--- iati/iati/settings/dev.py
+++ iati/iati/settings/dev.py
@@ -9,6 +9,7 @@ SECRET_KEY = '-sg0o=f6(j3!4u6^86!j@0&l^3clslh-#f@02d2^p_4vy0ma0y'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+ALLOWED_HOSTS = ['159.65.23.4']
 
 try:
     from .local import *
