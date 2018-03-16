a1d185be516e2fb4f7f8c303a6d2dc2188b526af
dalepotter <dalepotter@gmail.com>
2018-03-16 17:08:39 +0000

    Change variable name and add a note about how to overwrite the secret key

--- iati/iati/settings/dev.py
+++ iati/iati/settings/dev.py
@@ -4,6 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# Overwrite this variable in local.py with another unguessable string.
 SECRET_KEY = '-sg0o=f6(j3!4u6^86!j@0&l^3clslh-#f@02d2^p_4vy0ma0y'
 
 
