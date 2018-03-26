cda4fee26c6d145c2975aa40addb8caa76caff71
Alex Miller <alex.k.miller@gmail.com>
2018-03-26 10:51:57 +0100

    Explicit file upload permissions

--- iati/iati/settings/base.py
+++ iati/iati/settings/base.py
@@ -156,3 +156,6 @@ WAGTAIL_SITE_NAME = "iati"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+# Explicit file upload permissions so group can read
+FILE_UPLOAD_PERMISSIONS = "0o644"
