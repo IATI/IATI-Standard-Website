08892f6d71d57fa352e8996a835ff89941ef921d
Alex Miller <alex.k.miller@gmail.com>
2018-04-10 15:54:48 -0400

    Auto-creation of default pages, fixing of home page, and language selector include template

--- iati/home/management/commands/create_default_pages.py
+++ iati/home/management/commands/create_default_pages.py
@@ -6,47 +6,64 @@ from events.models import EventIndexPage
 from guidance_and_support.models import GuidanceAndSupportPage
 from news.models import NewsIndexPage
 
+from django.conf import settings
+
 class Command(BaseCommand):
     help = 'Create the default pages that constitute the skeleton of the website information architecture'
     def handle(self, *args, **options):
-        home_page = HomePage.objects.live().first()
+        home_page_queryset = HomePage.objects.live()
+        home_page = home_page_queryset.first()
         if home_page is not None:
-            home_page.update(url_path_en="/home/",title_en="Home",slug_en="home")
-        self.stdout.write(self.style.WARNING('Successfully fixed home page'))
-            
-        about_page = AboutPage.objects.live().first()
-        if about_page is None:
-            self.stdout.write(self.style.WARNING('No about page! Creating about page...'))
-            about_page = AboutPage(title_en="About",slug_en="about")
-            home_page.add_child(about_page)
-            about_page.save_revision().publish()
-            
-        contact_page = ContactPage.objects.live().first()
-        if contact_page is None:
-            self.stdout.write(self.style.WARNING('No contact page! Creating page...'))
-            contact_page = ContactPage(title_en="Contact",slug_en="contact")
-            home_page.add_child(contact_page)
-            contact_page.save_revision().publish()
-            
-        event_index_page = EventIndexPage.objects.live().first()
-        if event_index_page is None:
-            self.stdout.write(self.style.WARNING('No event index page! Creating page...'))
-            event_index_page = EventIndexPage(title_en="Events",slug_en="events")
-            home_page.add_child(event_index_page)
-            event_index_page.save_revision().publish()
-            
-        guidance_and_support_page = GuidanceAndSupportPage.objects.live().first()
-        if guidance_and_support_page is None:
-            self.stdout.write(self.style.WARNING('No guidance and support page! Creating page...'))
-            guidance_and_support_page = GuidanceAndSupportPage(title_en="Guidance and support",slug_en="guidance_and_support")
-            home_page.add_child(guidance_and_support_page)
-            guidance_and_support_page.save_revision().publish()
-            
-        news_index_page = NewsIndexPage.objects.live().first()
-        if news_index_page is None:
-            self.stdout.write(self.style.WARNING('No news page! Creating page...'))
-            news_index_page = NewsIndexPage(title_en="News",slug_en="news")
-            home_page.add_child(news_index_page)
-            news_index_page.save_revision().publish()
-            
-        self.stdout.write(self.style.SUCCESS('Successfully checked/created default pages...'))
+            home_page_queryset.update(url_path_en="/home/", url_path="/home/")
+            home_page.title_en = "Home"
+            home_page.slug_en = "home"
+            home_page.url_path_en = "/home/"
+            home_page.title = "Home"
+            home_page.slug = "home"
+            home_page.url_path = "/home/"
+
+            self.stdout.write(self.style.SUCCESS('Successfully fixed home page...'))
+
+            about_page = AboutPage.objects.live().first()
+            if about_page is None:
+                self.stdout.write(self.style.WARNING('No about page! Creating about page...'))
+                about_page = AboutPage(title_en="About", slug_en="about", title="About", slug="about")
+                home_page.add_child(instance=about_page)
+                about_page.save_revision().publish()
+                about_page.save()
+
+            contact_page = ContactPage.objects.live().first()
+            if contact_page is None:
+                self.stdout.write(self.style.WARNING('No contact page! Creating page...'))
+                contact_page = ContactPage(title_en="Contact", slug_en="contact", title="Contact", slug="contact")
+                home_page.add_child(instance=contact_page)
+                contact_page.save_revision().publish()
+                contact_page.save()
+
+            event_index_page = EventIndexPage.objects.live().first()
+            if event_index_page is None:
+                self.stdout.write(self.style.WARNING('No event index page! Creating page...'))
+                event_index_page = EventIndexPage(title_en="Events", slug_en="events", title="Events", slug="events")
+                home_page.add_child(instance=event_index_page)
+                event_index_page.save_revision().publish()
+                event_index_page.save()
+
+            guidance_and_support_page = GuidanceAndSupportPage.objects.live().first()
+            if guidance_and_support_page is None:
+                self.stdout.write(self.style.WARNING('No guidance and support page! Creating page...'))
+                guidance_and_support_page = GuidanceAndSupportPage(title_en="Guidance and support", slug_en="guidance_and_support", title="Guidance and support", slug="guidance_and_support")
+                home_page.add_child(instance=guidance_and_support_page)
+                guidance_and_support_page.save_revision().publish()
+                guidance_and_support_page.save()
+
+            news_index_page = NewsIndexPage.objects.live().first()
+            if news_index_page is None:
+                self.stdout.write(self.style.WARNING('No news page! Creating page...'))
+                news_index_page = NewsIndexPage(title_en="News", slug_en="news", title="News", slug="news")
+                home_page.add_child(instance=news_index_page)
+                news_index_page.save_revision().publish()
+                news_index_page.save()
+
+            home_page.save()
+
+            self.stdout.write(self.style.SUCCESS('Successfully checked/created default pages.'))
--- iati/home/templatetags/iati_tags.py
+++ iati/home/templatetags/iati_tags.py
@@ -1,15 +1,35 @@
 import os
 from django import template
+from home.models import HomePage
+from about.models import AboutPage
+from contact.models import ContactPage
 from events.models import EventIndexPage
+from guidance_and_support.models import GuidanceAndSupportPage
+from news.models import NewsIndexPage
+
+from django.conf import settings
+from wagtail_modeltranslation.contextlib import use_language
+from wagtail.core.templatetags.wagtailcore_tags import pageurl
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def events_index_page_url(context):
-    """Returns the relative url for the events index page"""
-    events_index_page = EventIndexPage.objects.live().first()
-    if events_index_page is not None:
-        return events_index_page.get_url(context['request'])
+def default_page_url(context, default_page_name="home"):
+    """Returns the relative url for a top-level default page"""
+    if default_page_name == "about":
+        default_page = AboutPage.objects.live().first()
+    elif default_page_name == "contact":
+        default_page = ContactPage.objects.live().first()
+    elif default_page_name == "events":
+        default_page = EventIndexPage.objects.live().first()
+    elif default_page_name == "guidance_and_support":
+        default_page = GuidanceAndSupportPage.objects.live().first()
+    elif default_page_name == "news":
+        default_page = NewsIndexPage.objects.live().first()
+    else:
+        default_page = HomePage.objects.live().first()
+    if default_page is not None:
+        return default_page.get_url(context['request'])
     return ""
 
 def humansize(nbytes):
@@ -26,3 +46,16 @@ def humansize(nbytes):
 def filesize(value):
     """Returns the filesize of the filename given in value"""
     return humansize(os.path.getsize(value))
+
+@register.inclusion_tag("home/includes/translation_links.html",takes_context=True)
+def translation_links(context, calling_page):
+    """Takes the inclusion template 'translation_links.html' and returns a snippet of HTML with links to the requesting page in all offered languages"""
+    language_results = []
+    for language_code, language_name in settings.LANGUAGES:
+        with use_language(language_code):
+            language_url = pageurl(context,calling_page)
+            language_results.append({"code": language_code, "name": language_name, "url": language_url})
+
+    return {
+        'languages': language_results,
+    }
