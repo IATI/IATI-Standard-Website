26b25482c244b232b85acbc36839f2243efbb560
allthatilk <allthatilk@users.noreply.github.com>
2018-04-11 16:36:10 +0100

    Add to page models and edit templates, update translation.py

--- iati/about/models.py
+++ iati/about/models.py
@@ -1,17 +1,38 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+
+from home.models import IATIStreamBlock
+
 
 class AboutPage(Page):
     parent_page_types = ['home.HomePage']
-    # subpage_types = ['about.AboutSubpage']
+    subpage_types = ['about.AboutSubPage']
+
+    heading = models.CharField(max_length=255, null=True, blank=True)
+    excerpt = models.TextField(null=True, blank=True)
+    content_editor = StreamField(IATIStreamBlock(required=False), null=True, blank=True)
+
+
+class AboutSubPage(Page):
+    parent_page_types = ['about.AboutPage']
+    subpage_types = []
+
+    heading = models.CharField(max_length=255, null=True, blank=True)
+    excerpt = models.TextField(null=True, blank=True)
+    content_editor = StreamField(IATIStreamBlock(required=False), null=True, blank=True)
+
+class CaseStudyPage(Page):
+    parent_page_types = ['about.AboutSubPage']
+    subpage_types = []
 
 #fffunction notes:
 
 # Model would be something like:
 #
-# title
-# intro (text area)
+## title
+## intro (text area)
 # content_editor
 # The members assembly block in the sidebar is an optional item, the model for which should be something like:
 #
--- iati/about/translation.py
+++ iati/about/translation.py
@@ -1,11 +1,23 @@
-from .models import AboutPage
+from .models import AboutPage, AboutSubPage
 from modeltranslation.translator import TranslationOptions
 from modeltranslation.decorators import register
+
+# from home.translation_helper import add_language_content_panels
 
 
 @register(AboutPage)
 class AboutPageTR(TranslationOptions):
-    pass
-    # fields = (
-    #     'body',
-    # )
+    fields = (
+        'heading',
+        'excerpt',
+        'content_editor',
+    )
+
+
+@register(AboutSubPage)
+class AboutSubPageTR(TranslationOptions):
+    fields = (
+        'heading',
+        'excerpt',
+        'content_editor',
+    )
