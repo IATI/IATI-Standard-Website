c30c3ca1af49fb38e6b65ef1a43c36e68377f8d7
allthatilk <allthatilk@users.noreply.github.com>
2018-04-12 17:43:11 +0100

    WIP Modifying case study page templates and setting up models

--- iati/about/models.py
+++ iati/about/models.py
@@ -6,26 +6,44 @@ from wagtail.core.fields import StreamField
 from home.models import IATIStreamBlock
 
 
-class AboutPage(Page):
+class AbstractAboutPage(Page):
+    """
+    TODO:
+        Decide what to call this and where it should live.
+        Create abstract Index page and abstract Content page.
+
+    """
+    heading = models.CharField(max_length=255, null=True, blank=True)
+    excerpt = models.TextField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class AboutPage(AbstractAboutPage):
     parent_page_types = ['home.HomePage']
     subpage_types = ['about.AboutSubPage']
 
-    heading = models.CharField(max_length=255, null=True, blank=True)
-    excerpt = models.TextField(null=True, blank=True)
     content_editor = StreamField(IATIStreamBlock(required=False), null=True, blank=True)
 
 
-class AboutSubPage(Page):
+class AboutSubPage(AbstractAboutPage):
     parent_page_types = ['about.AboutPage']
     subpage_types = []
 
-    heading = models.CharField(max_length=255, null=True, blank=True)
-    excerpt = models.TextField(null=True, blank=True)
     content_editor = StreamField(IATIStreamBlock(required=False), null=True, blank=True)
 
-# class CaseStudyPage(Page):
-#     parent_page_types = ['about.AboutSubPage']
-#     subpage_types = []
+
+class CaseStudiesIndexPage(AbstractAboutPage):
+    parent_page_types = ['about.AboutPage']
+    subpage_types = ['about.CaseStudyPage']
+
+
+class CaseStudyPage(AbstractAboutPage):
+    parent_page_types = ['about.CaseStudiesIndexPage']
+    subpage_types = []
+
+    content_editor = StreamField(IATIStreamBlock(required=False), null=True, blank=True)
 
 #fffunction notes:
 
