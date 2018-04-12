4c15bd51b76da56f2cab788c8d36cc6fc2b5f21b
dalepotter <dalepotter@gmail.com>
2018-04-12 16:06:50 +0100

    Add basic model for the contact page

--- iati/contact/models.py
+++ iati/contact/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-
+from home.models import IATIStreamBlock
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 
 class ContactPage(Page):
-    pass
+    parent_page_types = ['home.HomePage']
+    subpage_types = []
+
+    subheading = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subheading', classname="full"),
+        FieldPanel('description', classname="full")
+    ]
