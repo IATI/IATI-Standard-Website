34f0148470f2939725a214b89b5c303339e64984
allthatilk <allthatilk@users.noreply.github.com>
2018-04-13 15:21:55 +0100

    Finalise Abstract page models and rename class

--- iati/home/models.py
+++ iati/home/models.py
@@ -4,27 +4,9 @@ from wagtail.core.models import Page
 from django.utils import translation
 
 from wagtail.core.blocks import TextBlock, StructBlock, StreamBlock, FieldBlock, CharBlock, RichTextBlock, RawHTMLBlock
+from wagtail.core.fields import StreamField
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.documents.blocks import DocumentChooserBlock
-
-
-class AbstractContentPage(Page):
-    """A base for the basic model blocks of all content type pages."""
-    heading = models.CharField(max_length=255, null=True, blank=True)
-    excerpt = models.TextField(null=True, blank=True)
-    content_editor = StreamField(IATIStreamBlock(required=False), null=True, blank=True)
-
-    class Meta:
-        abstract = True
-
-
-class AbstractIndexPage(Page):
-    """"A base for the basic model block of all index type pages."""
-    heading = models.CharField(max_length=255, null=True, blank=True)
-    excerpt = models.TextField(null=True, blank=True)
-
-    class Meta:
-        abstract = True
 
 
 class PullQuoteBlock(StructBlock):
@@ -34,14 +16,17 @@ class PullQuoteBlock(StructBlock):
     class Meta:
         icon = "openquote"
 
+
 class EndNoteBlock(StructBlock):
     number = CharBlock()
     citation = RichTextBlock(required=False)
+
 
 class HTMLAlignmentChoiceBlock(FieldBlock):
     field = forms.ChoiceField(choices=(
         ('normal', 'Normal'), ('full', 'Full width'),
     ))
+
 
 class AlignedHTMLBlock(StructBlock):
     html = RawHTMLBlock()
@@ -50,15 +35,18 @@ class AlignedHTMLBlock(StructBlock):
     class Meta:
         icon = "code"
 
+
 class ImageFormatChoiceBlock(FieldBlock):
     field = forms.ChoiceField(choices=(
         ('left', 'Wrap left'), ('right', 'Wrap right'), ('mid', 'Mid width'), ('full', 'Full width'),
     ))
 
+
 class ImageBlock(StructBlock):
     image = ImageChooserBlock()
     caption = RichTextBlock(required=False)
     alignment = ImageFormatChoiceBlock()
+
 
 class IATIStreamBlock(StreamBlock):
     h2 = CharBlock(icon="title", classname="title")
@@ -71,6 +59,30 @@ class IATIStreamBlock(StreamBlock):
     pullquote = PullQuoteBlock()
     aligned_html = AlignedHTMLBlock(icon="code", label='Raw HTML')
     document = DocumentChooserBlock(icon="doc-full-inverse")
+
+
+class AbstractBasePage(Page):
+    """A base for all page types."""
+    heading = models.CharField(max_length=255, null=True, blank=True)
+    excerpt = models.TextField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class AbstractContentPage(AbstractBasePage):
+    """A base for the basic model blocks of all content type pages."""
+    content_editor = StreamField(IATIStreamBlock(required=False), null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class AbstractIndexPage(AbstractBasePage):
+    """"A base for the basic model block of all index type pages."""
+    class Meta:
+        abstract = True
+
 
 class HomePage(Page):
     pass
