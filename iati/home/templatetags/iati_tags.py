591ff6dc6af99a59c5d61a445a39a9de6440c217
Alex Miller <alex.k.miller@gmail.com>
2018-04-16 11:44:31 -0400

    Remove event specific tags

--- iati/home/templatetags/iati_tags.py
+++ iati/home/templatetags/iati_tags.py
@@ -3,7 +3,7 @@ from django import template
 from home.models import HomePage
 from about.models import AboutPage
 from contact.models import ContactPage
-from events.models import EventIndexPage, EventType
+from events.models import EventIndexPage
 from guidance_and_support.models import GuidanceAndSupportPage
 from news.models import NewsIndexPage
 
@@ -47,38 +47,3 @@ def translation_links(context, calling_page):
     return {
         'languages': language_results,
     }
-
-@register.filter
-def haspassed(value):
-    """Takes a date and tells you if it's in the past"""
-    now = timezone.now()
-    return value < now
-
-@register.filter
-def twopartdate(date_start, date_end):
-    """Takes two datetimes and determines whether to display start and end times, or start and end dates.
-
-    If an end date exists, we can compare the two dates.
-    If the two dates are the same, localize the date for the first part and stringify the time range for the second.
-    If the two dates are not the same, part 2 becomes the second date.
-
-    If no end date exists, part 2 is just the start time.
-    """
-    part1 = _date(date_start, "DATE_FORMAT")
-    enDash = u'\u2013'
-    if date_end:
-        if date_start.date() == date_end.date():
-            part2 = "{0}{1}{2}".format(_date(date_start, "TIME_FORMAT"), enDash, _date(date_end, "TIME_FORMAT"))
-            part2_is_time = True
-        else:
-            part2 = _date(date_end, "DATE_FORMAT")
-            part2_is_time = False
-    else:
-        part2 = _date(date_start, "TIME_FORMAT")
-        part2_is_time = True
-    return {"part1":part1, "part2":part2, "part2_is_time":part2_is_time}
-
-@register.filter
-def event_type_verbose(event_type_slug):
-    """Returns the localized event type name given a slug"""
-    return EventType.objects.get(slug=event_type_slug).name
