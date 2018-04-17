3934c5a319c400f59fb35711491f4121c0d7c0b3
Alex Miller <alex.k.miller@gmail.com>
2018-04-17 09:58:23 -0400

    Don't need copy

--- iati/events/translation.py
+++ iati/events/translation.py
@@ -6,16 +6,16 @@ from home.translation_helper import add_language_content_panels
 
 @register(EventIndexPage)
 class EventIndexPageTR(TranslationOptions):
-    fields = EventIndexPage.translation_fields.copy()
+    fields = EventIndexPage.translation_fields
 add_language_content_panels(EventIndexPage)
 
 
 @register(EventPage)
 class EventPageTR(TranslationOptions):
-    fields = EventPage.translation_fields.copy()
+    fields = EventPage.translation_fields
 add_language_content_panels(EventPage)
 
 
 @register(EventType)
 class EventTypeTR(TranslationOptions):
-    fields = EventType.translation_fields.copy()
+    fields = EventType.translation_fields
