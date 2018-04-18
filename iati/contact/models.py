15266497c69fafa2b3583fc14fc76a98ae41e944
dalepotter <dalepotter@gmail.com>
2018-04-18 15:02:30 +0100

    Adapt contact page model to use abstract pages
    This also dynamically adds language content panels to the CMS for this model.

--- iati/contact/models.py
+++ iati/contact/models.py
@@ -1,17 +1,9 @@
 from django.db import models
-from home.models import IATIStreamBlock
+from home.models import AbstractContentPage
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 
-class ContactPage(Page):
+class ContactPage(AbstractContentPage):
     parent_page_types = ['home.HomePage']
     subpage_types = []
-
-    subheading = models.TextField(null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-
-    content_panels = Page.content_panels + [
-        FieldPanel('subheading', classname="full"),
-        FieldPanel('description', classname="full")
-    ]
