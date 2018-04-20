74f21da7d63a8548e194bcb3fbcd97b2df31f667
Alex Miller <alex.k.miller@gmail.com>
2018-04-20 13:26:31 -0400

    Stop warning about update

--- iati/iati/settings/base.py
+++ iati/iati/settings/base.py
@@ -171,6 +171,7 @@ MEDIA_URL = '/media/'
 # Wagtail settings
 
 WAGTAIL_SITE_NAME = "iati"
+WAGTAIL_ENABLE_UPDATE_CHECK=False
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
