84466e0f6ef245d2684b002f2535fb7bb0918f22
Alex Miller <alex.k.miller@gmail.com>
2018-04-25 09:18:31 -0400

    Children of people

--- iati/about/models.py
+++ iati/about/models.py
@@ -145,7 +145,7 @@ class PeopleProfileBlock(StreamBlock):
 class PeoplePage(AbstractContentPage):
     """A model for the People page."""
 
-    subpage_types = []
+    subpage_types = ["about.PeoplePage", "about.AbooutSubPage"]
 
     profile_content_editor = StreamField(PeopleProfileBlock, null=True, blank=True)
 
