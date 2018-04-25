510bbd2eef6a28b65748437493a5307c930d5607
Alex Miller <alex.k.miller@gmail.com>
2018-04-25 08:25:43 -0400

    Replace unused unpacked variable with underscore

--- iati/iati/activate_languages.py
+++ iati/iati/activate_languages.py
@@ -15,7 +15,7 @@ class ActiveLocalePrefixPattern(LocalePrefixPattern):
         This allows us to check for activated languages before resolving URL.
         """
         language_code = get_language() or settings.LANGUAGE_CODE
-        if language_code not in [active_language_code for active_language_code, active_language_name in settings.ACTIVE_LANGUAGES]:
+        if language_code not in [active_language_code for active_language_code, _ in settings.ACTIVE_LANGUAGES]:
             language_code = settings.LANGUAGE_CODE
             activate(language_code)
         if language_code == settings.LANGUAGE_CODE and not self.prefix_default_language:
