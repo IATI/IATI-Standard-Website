1115db639e14c914daa9152d9bcc459f3aa4aacc
allthatilk <allthatilk@users.noreply.github.com>
2018-05-01 15:59:22 +0100

    Check home page url and title display as expected

--- iati/tests/functional_tests.py
+++ iati/tests/functional_tests.py
@@ -1,10 +1,16 @@
 import pytest
 
 
-def test_multiple_browsers(browser):
-    """A test to test you can test with multiple browsers at once."""
-    browser.visit('https://www.google.com')
-    assert browser.url == 'https://www.google.com/'
-    browser.quit()
+localhost = 'http://127.0.0.1:8000/'
+# def test_multiple_browsers(browser):
+#     """A test to test you can test with multiple browsers at once."""
+#     browser.visit('https://www.google.com')
+#     assert browser.url == 'https://www.google.com/'
+#     browser.quit()
 
-# def test_default_pages_exist(available_browser)
+def test_home_page_exists(browser):
+    """A test to check for the existence of the home page."""
+    browser.visit(localhost)
+    # assert browser.url == localhost + 'en/'
+    assert browser.title == 'Home'
+    browser.quit()
