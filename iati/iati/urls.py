238061b9aae11bd4a3a3e3025c8c8f4f675a867d
dalepotter <dalepotter@gmail.com>
2018-05-02 11:24:44 +0100

    Linting: Fix order of import issues

--- iati/iati/urls.py
+++ iati/iati/urls.py
@@ -6,13 +6,11 @@ from django.conf.urls.i18n import i18n_patterns  # For internationalization
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
-
-from search import views as search_views
-
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
+from search import views as search_views
 
 urlpatterns = [
     url(r'^django-admin/', admin.site.urls),
