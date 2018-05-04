11cd3e19fade131d872a34c3f4801bbcd90cbc25
allthatilk <allthatilk@users.noreply.github.com>
2018-05-04 12:49:15 +0100

    Add test to check default pages exist

--- iati/tests/base_functional_tests.py
+++ iati/tests/base_functional_tests.py
@@ -1,1 +1,20 @@
 """A module of functional tests for base site functionality."""
+import pytest
+from conftest import LOCALHOST
+
+
+DEFAULT_PAGE_NAMES = [
+    'about',
+    'contact',
+    'events',
+    'news',
+    'guidance_and_support'
+]
+
+
+@pytest.mark.parametrize("page_name", DEFAULT_PAGE_NAMES)
+def test_default_pages_exist(browser, page_name):
+    """Check default pages exist."""
+    browser.visit(LOCALHOST + '{}'.format(page_name))
+    page_title = page_name.replace('_', ' ').capitalize()
+    assert browser.title == page_title
