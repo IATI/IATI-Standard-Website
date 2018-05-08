0671e3eac2b7fac0dd8f549ccf4f160850f8d27b
allthatilk <allthatilk@users.noreply.github.com>
2018-05-08 16:22:28 +0100

    refactor current tests to parametrize page types and titles to use one test

--- iati/tests/about_functional_tests.py
+++ iati/tests/about_functional_tests.py
@@ -1,33 +1,26 @@
 """A module of functional tests for the about page and its sub pages."""
-# import pytest
+import pytest
 
 
 # @pytest.mark.django_db()
 class TestAboutChildPageCreation():
     """A container for tests to check the ability to creat About child pages."""
 
-    def test_can_create_about_sub_page(self, admin_browser):
-        """Check that an about sub page can be created for the About page."""
+    @pytest.mark.parametrize('child_page', [
+        {'page_type': 'About sub page', 'title': 'test sub page'},
+        {'page_type': 'Case study index page', 'title': 'test case study index page'},
+        {'page_type': 'History page', 'title': 'test history page'},
+        {'page_type': 'People page', 'title': 'test people page'}
+    ])
+    def test_can_create_about_child_pages(self, admin_browser, child_page):
+        """Check that expected about child pages can be created."""
         admin_browser.click_link_by_text('Pages')
         admin_browser.find_by_xpath('//*[@class="icon icon-arrow-right "]').click()
         admin_browser.find_by_text('About').click()
         admin_browser.find_by_text('Add child page').click()
-        admin_browser.find_by_text('About sub page').click()
+        admin_browser.find_by_text(child_page['page_type']).click()
         admin_browser.find_by_text('English').click()
-        admin_browser.fill('title_en', 'test sub page')
+        admin_browser.fill('title_en', child_page['title'])
         admin_browser.find_by_xpath('//*[@class="dropdown-toggle icon icon-arrow-up"]').click()
         admin_browser.find_by_text('Publish').click()
-        assert admin_browser.find_by_text('test sub page')
-
-    def test_can_create_case_study_index_page(self, admin_browser):
-        """Check that a case study index page can be created for the About page."""
-        admin_browser.click_link_by_text('Pages')
-        admin_browser.find_by_xpath('//*[@class="icon icon-arrow-right "]').click()
-        admin_browser.find_by_text('About').click()
-        admin_browser.find_by_text('Add child page').click()
-        admin_browser.find_by_text('Case study index page').click()
-        admin_browser.find_by_text('English').click()
-        admin_browser.fill('title_en', 'test case study index page')
-        admin_browser.find_by_xpath('//*[@class="dropdown-toggle icon icon-arrow-up"]').click()
-        admin_browser.find_by_text('Publish').click()
-        assert admin_browser.find_by_text('test case study index page')
+        assert admin_browser.find_by_text(child_page['title'])
