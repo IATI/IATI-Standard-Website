6994496b9c105d27792e5c9cd386a2a1362cf679
allthatilk <allthatilk@users.noreply.github.com>
2018-05-08 15:34:52 +0100

    Update line to prevent version error associated with visually hidden object

--- iati/tests/about_functional_tests.py
+++ iati/tests/about_functional_tests.py
@@ -9,7 +9,7 @@ class TestAboutChildPageCreation():
     def test_can_create_about_sub_page(self, admin_browser):
         """Check that an about sub page can be created for the About page."""
         admin_browser.click_link_by_text('Pages')
-        admin_browser.find_by_text('See children').click()
+        admin_browser.find_by_xpath('//*[@class="icon icon-arrow-right "]').click()
         admin_browser.find_by_text('About').click()
         admin_browser.find_by_text('Add child page').click()
         admin_browser.find_by_text('About sub page').click()
@@ -22,7 +22,7 @@ class TestAboutChildPageCreation():
     def test_can_create_case_study_index_page(self, admin_browser):
         """Check that a case study index page can be created for the About page."""
         admin_browser.click_link_by_text('Pages')
-        admin_browser.find_by_text('See children').click()
+        admin_browser.find_by_xpath('//*[@class="icon icon-arrow-right "]').click()
         admin_browser.find_by_text('About').click()
         admin_browser.find_by_text('Add child page').click()
         admin_browser.find_by_text('Case study index page').click()
