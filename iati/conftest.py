48334995088ea90f817893bb3393ef317576fcac
Alex Miller <alex.k.miller@gmail.com>
2018-05-08 14:46:13 -0400

    Need a live server, but long-standing wagtail bug is preventing tests from passing

--- iati/conftest.py
+++ iati/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from splinter import Browser
+from django.core.management import call_command
 from iati.settings.local import DJANGO_ADMIN_USER, DJANGO_ADMIN_PASS
+from django.contrib.auth.models import User
 
 LOCALHOST = 'http://127.0.0.1:8000/'
 
@@ -15,11 +17,20 @@ def multibrowser(request):
 
 
 @pytest.fixture(scope='function')
-def admin_browser(browser):
+def admin_browser(browser, live_server):
     """Create a browser that is logged in to the CMS."""
-    browser.visit(LOCALHOST+'admin/')
+    browser.visit(live_server.url+'/admin/')
     browser.fill('username', DJANGO_ADMIN_USER)
     browser.fill('password', DJANGO_ADMIN_PASS)
     sign_in_button = browser.find_by_css("button").first
     sign_in_button.click()
     return browser
+
+
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        if not User.objects.filter(username=DJANGO_ADMIN_USER).exists():
+            User.objects.create_superuser(DJANGO_ADMIN_USER, "admin@email.com", DJANGO_ADMIN_PASS)
+        call_command('createdefaultpages')
+    return django_db_setup
--- iati/tests/about_functional_tests.py
+++ iati/tests/about_functional_tests.py
@@ -1,11 +1,10 @@
 """A module of functional tests for the about page and its sub pages."""
-# import pytest
+import pytest
 
 
-# @pytest.mark.django_db()
+@pytest.mark.django_db()
 class TestAboutChildPageCreation():
     """A container for tests to check the ability to creat About child pages."""
-
     def test_can_create_about_sub_page(self, admin_browser):
         """Check that an about sub page can be created for the About page."""
         admin_browser.click_link_by_text('Pages')
@@ -15,6 +14,8 @@ class TestAboutChildPageCreation():
         admin_browser.find_by_text('About sub page').click()
         admin_browser.find_by_text('English').click()
         admin_browser.fill('title_en', 'test sub page')
+        admin_browser.find_by_text('Promote').click()
+        admin_browser.fill('slug_en', 'test-sub-page')
         admin_browser.find_by_xpath('//*[@class="dropdown-toggle icon icon-arrow-up"]').click()
         admin_browser.find_by_text('Publish').click()
         assert admin_browser.find_by_text('test sub page')
@@ -28,6 +29,8 @@ class TestAboutChildPageCreation():
         admin_browser.find_by_text('Case study index page').click()
         admin_browser.find_by_text('English').click()
         admin_browser.fill('title_en', 'test case study index page')
+        admin_browser.find_by_text('Promote').click()
+        admin_browser.fill('slug_en', 'test-case-study-index-page')
         admin_browser.find_by_xpath('//*[@class="dropdown-toggle icon icon-arrow-up"]').click()
         admin_browser.find_by_text('Publish').click()
         assert admin_browser.find_by_text('test case study index page')
