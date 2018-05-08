713cc6b3e25e67eab27c258315d6efd0663cc630
allthatilk <allthatilk@users.noreply.github.com>
2018-05-08 15:11:43 +0100

    Add test to check case study index page can be created as a child of the about page

--- iati/tests/about_functional_tests.py
+++ iati/tests/about_functional_tests.py
@@ -18,3 +18,16 @@ class TestAboutChildPageCreation():
         admin_browser.find_by_xpath('//*[@class="dropdown-toggle icon icon-arrow-up"]').click()
         admin_browser.find_by_text('Publish').click()
         assert admin_browser.find_by_text('test sub page')
+
+    def test_can_create_case_study_index_page(self, admin_browser):
+        """Check that a case study index page can be created for the About page."""
+        admin_browser.click_link_by_text('Pages')
+        admin_browser.find_by_text('See children').click()
+        admin_browser.find_by_text('About').click()
+        admin_browser.find_by_text('Add child page').click()
+        admin_browser.find_by_text('Case study index page').click()
+        admin_browser.find_by_text('English').click()
+        admin_browser.fill('title_en', 'test case study index page')
+        admin_browser.find_by_xpath('//*[@class="dropdown-toggle icon icon-arrow-up"]').click()
+        admin_browser.find_by_text('Publish').click()
+        assert admin_browser.find_by_text('test case study index page')
