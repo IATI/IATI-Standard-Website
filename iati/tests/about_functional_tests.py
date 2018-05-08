0430bcd303d0f66a57c9abcf232ff49ffee2a839
allthatilk <allthatilk@users.noreply.github.com>
2018-05-08 14:59:08 +0100

    Create class container for child page creation tests

--- iati/tests/about_functional_tests.py
+++ iati/tests/about_functional_tests.py
@@ -3,15 +3,18 @@
 
 
 # @pytest.mark.django_db()
-def test_can_create_about_sub_page(admin_browser):
-    """Check that an about sub page can be created for the About page."""
-    admin_browser.click_link_by_text('Pages')
-    admin_browser.find_by_text('See children').click()
-    admin_browser.find_by_text('About').click()
-    admin_browser.find_by_text('Add child page').click()
-    admin_browser.find_by_text('About sub page').click()
-    admin_browser.find_by_text('English').click()
-    admin_browser.fill('title_en', 'test sub page')
-    admin_browser.find_by_xpath('//*[@class="dropdown-toggle icon icon-arrow-up"]').click()
-    admin_browser.find_by_text('Publish').click()
-    assert admin_browser.find_by_text('test sub page')
+class TestAboutChildPageCreation():
+    """A container for tests to check the ability to creat About child pages."""
+
+    def test_can_create_about_sub_page(self, admin_browser):
+        """Check that an about sub page can be created for the About page."""
+        admin_browser.click_link_by_text('Pages')
+        admin_browser.find_by_text('See children').click()
+        admin_browser.find_by_text('About').click()
+        admin_browser.find_by_text('Add child page').click()
+        admin_browser.find_by_text('About sub page').click()
+        admin_browser.find_by_text('English').click()
+        admin_browser.fill('title_en', 'test sub page')
+        admin_browser.find_by_xpath('//*[@class="dropdown-toggle icon icon-arrow-up"]').click()
+        admin_browser.find_by_text('Publish').click()
+        assert admin_browser.find_by_text('test sub page')
