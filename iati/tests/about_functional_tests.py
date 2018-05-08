58cf51448d238e6d2848df1da8bd6ffc0162886a
allthatilk <allthatilk@users.noreply.github.com>
2018-05-08 14:38:37 +0100

    test about page can be created via user CMS

--- iati/tests/about_functional_tests.py
+++ iati/tests/about_functional_tests.py
@@ -2,6 +2,7 @@
 # import pytest
 
 
+# @pytest.mark.django_db()
 def test_can_create_about_sub_page(admin_browser):
     """Check that an about sub page can be created for the About page."""
     admin_browser.click_link_by_text('Pages')
@@ -11,6 +12,6 @@ def test_can_create_about_sub_page(admin_browser):
     admin_browser.find_by_text('About sub page').click()
     admin_browser.find_by_text('English').click()
     admin_browser.fill('title_en', 'test sub page')
-    # admin_browser.find_by_text('Save draft').click()
-    # import pdb; pdb.set_trace()
+    admin_browser.find_by_xpath('//*[@class="dropdown-toggle icon icon-arrow-up"]').click()
     admin_browser.find_by_text('Publish').click()
+    assert admin_browser.find_by_text('test sub page')
