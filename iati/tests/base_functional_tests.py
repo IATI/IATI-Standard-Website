5ab9767e70f85b64d21fd45660293e96a6eb0797
Alex Miller <alex.k.miller@gmail.com>
2018-05-11 12:19:46 -0400

    Test for idempotence

--- iati/tests/base_functional_tests.py
+++ iati/tests/base_functional_tests.py
@@ -1,6 +1,23 @@
 """A module of functional tests for base site functionality."""
+import os
 import pytest
 from conftest import LOCALHOST
+from django.core.management import call_command
+
+
+@pytest.mark.django_db()
+class TestCreateDefaultPages():
+    """A container for tests that check createdefaultpages command."""
+
+    def test_create_default_pages_idempotence(self, browser):
+        """Check pages have URLs after running command a second time after initial setup."""
+        browser.visit(os.environ["LIVE_SERVER_URL"])
+        browser.click_link_by_text("About")
+        valid_url = browser.url
+        call_command("createdefaultpages")
+        browser.visit(os.environ["LIVE_SERVER_URL"])
+        browser.click_link_by_text("About")
+        assert browser.url == valid_url
 
 
 class TestDefaultPagesExist():
