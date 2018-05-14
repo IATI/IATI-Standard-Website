281ecc3c51876a6eeb910efe315f0355124ae933
dalepotter <dalepotter@gmail.com>
2018-05-14 17:41:07 +0100

    Add Django admin settings

--- iati/iati/settings/dev.py
+++ iati/iati/settings/dev.py
@@ -14,6 +14,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ALLOWED_HOSTS = ['*']
 
+DJANGO_ADMIN_USER = 'testuser'
+DJANGO_ADMIN_PASS = 'password'
+
 try:
     from .local import *  # # noqa: F401, F403  # pylint: disable=unused-wildcard-import, wildcard-import
 except ImportError:
