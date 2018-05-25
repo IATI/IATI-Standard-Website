0fe0d57770a91afc5833d1ee90e0272510cc3d56
allthatilk <allthatilk@users.noreply.github.com>
2018-05-25 14:29:27 +0100

    Update GuidanceGroupPage class with changes since Alpha
    Update docstrings
    Update help text
    Change name from GuidanceIndexPage to GuidanceGroupPage
    Get GuidanceGroupPage to inherit from AbstractContentPage instead of AbstractIndex Page
    Add more detail to the property docstring
    Update variable names to reflect changes to GuidanceGroupPage name

--- iati/guidance_and_support/models.py
+++ iati/guidance_and_support/models.py
@@ -1,25 +1,27 @@
 from django.db import models
 
 from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from home.models import AbstractContentPage, AbstractIndexPage, IATIStreamBlock
 
 
 class GuidanceAndSupportPage(AbstractContentPage):
-    """"""
+    """A base for the Guidance and Support page."""
     parent_page_types = ['home.HomePage']
-    subpage_types = ['guidance_and_support.GuidanceIndexPage', 'guidance_and_support.KnowledgebaseIndexPage']
+    subpage_types = ['guidance_and_support.GuidanceGroupPage', 'guidance_and_support.KnowledgebaseIndexPage']
 
     @property
-    def guidance_indexes(self):
-        """Get all CaseStudyPage objects that have been published."""
-        guidance_indexes = GuidanceIndexPage.objects.child_of(self).live()
-        return guidance_indexes
+    def guidance_groups(self):
+        """Get all GuidanceGroupPage objects that have been published."""
+        guidance_groups = GuidanceGroupPage.objects.child_of(self).live()
+        return guidance_groups
 
 
-class GuidanceIndexPage(AbstractIndexPage):
-    subpage_types = ['guidance_and_support.GuidanceIndexPage', 'guidance_and_support.GuidancePage']
+class GuidanceGroupPage(AbstractContentPage):
+    """A base for Guidance Group pages."""
+    subpage_types = ['guidance_and_support.GuidanceGroupPage', 'guidance_and_support.GuidancePage']
 
     feed_image = models.ForeignKey(
         'wagtailimages.Image',
@@ -27,14 +29,26 @@ class GuidanceIndexPage(AbstractIndexPage):
         blank=True,
         on_delete=models.SET_NULL,
         related_name='+',
-        help_text='This is the image that will be displayed for the guidance index page on the main guidance and support page.'
+        help_text='This is the image that will be displayed for the guidance index page on the main guidance and support page. Ignore if this page is being used as a sub-index page.'
     )
 
-    feed_content = StreamField(IATIStreamBlock(required=False), null=True, blank=True, help_text='A small amount of content to appear on the main page (e.g. bullet points)')
+    feed_content = StreamField(IATIStreamBlock(required=False), null=True, blank=True, help_text='A small amount of content to appear on the main page (e.g. bullet points). Ignore if this page is being used as a sub-index page.')
 
-    feed_button_text = models.TextField(max_length=255, null=True, blank=True)
+    feed_button_text = models.TextField(max_length=255, null=True, blank=True, help_text='The text to appear on the button of the main guidance and support page. Ignore if this page is being used as a sub-index page.')
 
-    translation_fields = AbstractIndexPage.translation_fields + ["feed_content", "feed_button_text"]
+    @property
+    def guidance_groups(self):
+        """Get all objects that are children of the instantiated GuidanceGroupPage.
+
+        Note:
+            These can be other guidance group pages or single guidance pages.
+
+        """
+        guidance_groups = Page.objects.child_of(self).specific().live()
+        guidance_group_list = [{"page": page, "count": len(page.get_children())} for page in guidance_groups]
+        return guidance_group_list
+
+    translation_fields = AbstractContentPage.translation_fields + ["feed_content", "feed_button_text"]
 
     multilingual_field_panels = [
         ImageChooserPanel('feed_image'),
