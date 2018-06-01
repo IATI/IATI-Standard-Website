a930efbbf4638ab29a637aa598ff5def6b694046
allthatilk <allthatilk@users.noreply.github.com>
2018-06-01 17:03:44 +0100

    Use click_obscured to try and make Travis happy

--- iati/tests/base_functional_tests.py
+++ iati/tests/base_functional_tests.py
@@ -209,9 +209,10 @@ class TestDefaultPages():
     def upload_an_image(self, admin_browser):
         """Upload an image in the CMS."""
         admin_browser.find_by_text('Choose an image').click()
-        admin_browser.find_by_text('Upload').click()
+        click_obscured(admin_browser, admin_browser.find_by_text('Upload').first)
         admin_browser.fill('title', 'Test image')
         admin_browser.attach_file('file', TEST_DATA_DIR + 'pigeons.jpeg')
+        # import pdb; pdb.set_trace()
         admin_browser.find_by_xpath('//em[contains(text(), "Upload")]').click()
 
     def publish_changes(self, admin_browser):
