2bc403970dd5482eb99f67310615a0dda320ccb2
Alex Miller <alex.k.miller@gmail.com>
2018-06-04 13:34:44 -0400

    Complete importer, probably just need to work on encoding of JSON

--- iati/home/management/commands/importnews.py
+++ iati/home/management/commands/importnews.py
@@ -1,8 +1,9 @@
 import json
+import datetime
 from django.utils.text import slugify
 from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ValidationError
 from news.models import NewsIndexPage, NewsPage
-from wagtail.tests.utils.form_data import streamfield
 
 
 class Command(BaseCommand):
@@ -22,21 +23,26 @@ class Command(BaseCommand):
 
         news_index_page = NewsIndexPage.objects.live().first()
         if news_index_page is not None:
-            with open(options['json_file']) as json_file:
-                json_data = json.loads(json_file)
+            with open(options['json_file'][0]) as json_file:
+                json_data = json.load(json_file)
 
                 for page_data in json_data:
-                    news_page = NewsPage(
-                        title_en=page_data["title"],
-                        slug_en=slugify(page_data["title"]),
-                        header_en=page_data["title"],
-                        content_editor_en=streamfield([('paragraph', page_data["content"]), ]),
-                        title=page_data["title"],
-                        slug=slugify(page_data["title"]),
-                        header=page_data["title"],
-                        content_editor=streamfield([('paragraph', page_data["content"]), ]),
-                    )
-                    news_index_page.add_child(instance=news_page)
-                    news_page.save_revision().publish()
+                    try:
+                        news_page = NewsPage(
+                            title_en=page_data["title"],
+                            slug_en=slugify(page_data["title"]),
+                            heading_en=page_data["title"],
+                            content_editor_en=json.dumps([{'type': 'paragraph', 'value': page_data["content"]}]),
+                            title=page_data["title"],
+                            slug=slugify(page_data["title"]),
+                            heading=page_data["title"],
+                            content_editor=json.dumps([{'type': 'paragraph', 'value': page_data["content"]}]),
+                            date=datetime.datetime.strptime(page_data["date"], "%Y-%m-%d").date()
+                        )
+                        news_index_page.add_child(instance=news_page)
+                        news_page.save_revision().publish()
+                        self.stdout.write(self.style.SUCCESS(page_data["title"]))
+                    except ValidationError:
+                        self.stdout.write(self.style.NOTICE(page_data["title"]))
 
             self.stdout.write(self.style.SUCCESS('Successfully imported news.'))
