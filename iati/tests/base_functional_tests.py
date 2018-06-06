7176e83083455118c350c80ede6e3e2b70d961d1
Alex Miller <alex.k.miller@gmail.com>
2018-06-06 14:58:03 -0400

    Lowercase S on Guidance and support title

--- iati/tests/base_functional_tests.py
+++ iati/tests/base_functional_tests.py
@@ -192,7 +192,7 @@ class TestDefaultPages():
         {'name': 'Contact', 'slug': 'contact'},
         {'name': 'Events', 'slug': 'events'},
         {'name': 'News', 'slug': 'news'},
-        {'name': 'Guidance and Support', 'slug': 'guidance_and_support'}
+        {'name': 'Guidance and support', 'slug': 'guidance_and_support'}
     ]
 
     def navigate_to_edit_home_page(self, admin_browser, default_page_name):
