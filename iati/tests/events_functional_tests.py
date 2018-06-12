2860db88e7bbdb0f63e4c5946a1d2904b27043f5
Alex Miller <alex.k.miller@gmail.com>
2018-06-12 13:58:28 -0400

    We really should have merged editable-header-image first...

--- iati/tests/events_functional_tests.py
+++ iati/tests/events_functional_tests.py
@@ -220,6 +220,7 @@ class TestEventPages():
 
     def test_feed_image_shows_on_index_page(self, admin_browser):
         """Check that when a user adds a feed image it also becomes the header image."""
+        admin_browser.visit(os.environ['LIVE_SERVER_URL'] + '/admin/pages/6/')
         admin_browser.find_by_text(EVENT_PAGE['title']).click()
         scroll_to_bottom_of_page(admin_browser)
         # import pdb; pdb.set_trace()
