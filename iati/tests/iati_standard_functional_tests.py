c943c0b7c0efd9677360891796c5412fb431d863
allthatilk <imogen.kutz@devinit.org>
2018-06-19 11:20:06 +0100

    Add app for new default iati-standard page

---
+++ iati/tests/iati_standard_functional_tests.py
@@ -0,0 +1,7 @@
+# I want to visit the IATI Standard page
+# I want to be able to edit the header image of this page
+# I want to be able to edit the header text of this page
+# I want to be able to edit the excerpt of this page
+# I want to be able to add summary content to this page
+# I want to be directed to the appropriate section of the site for me
+# I want to be able to navigate to the appropriate page of the old Standard website
