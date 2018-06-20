c17dff62af821a2bf3f446e93d574a23abbb3968
Alex Miller <alex.k.miller@gmail.com>
2018-06-20 13:18:04 -0400

    Linting, adding standard page to iati_tags for base.html linking, no need for full URL in static link.

--- iati/iati_standard/models.py
+++ iati/iati_standard/models.py
@@ -1,5 +1,4 @@
 """A module for the IATI Standard page model."""
-from django.db import models
 from home.models import AbstractContentPage
 
 
@@ -7,4 +6,3 @@ class IATIStandardPage(AbstractContentPage):
     """A model for the IATI Standard Page, a landing page for IATI reference."""
     parent_page_types = ['home.HomePage']
     subpage_types = []
-    translation_fields = AbstractContentPage.translation_fields
