ee2eca87c2e0087bafd3e15437fb2f35122f4255
allthatilk <imogen.kutz@devinit.org>
2018-06-20 17:12:08 +0100

    Add parametrized test for external web links to old standard site, implement

--- iati/tests/iati_standard_functional_tests.py
+++ iati/tests/iati_standard_functional_tests.py
@@ -97,15 +97,43 @@ class TestIATIStandardPageisEditable():
 
 
 # I want to be directed to the appropriate section of the old IATI standard site for me
-# @pytest.mark.django_db
-# class TestRedirectLinksWorking():
-#     """Container for tests that check redirects are correct and working."""
-#
-#     ORG_STANDARD = {'id': 'org-standard', 'expected_content': 'This section details the IATI Organisation standard.'}
-#
-#     HARDCODED_LINKS = [ORG_STANDARD]
-#
-#     @pytest.mark.parametrize('link', HARDCODED_LINKS)
-#     def test_hardcoded_links_work(self, admin_browser, link):
-#         """Check that the given link goes to the correct page."""
-#         admin_browser
+@pytest.mark.django_db
+class TestRedirectLinksWorking():
+    """Container for tests that check redirects are correct and working."""
+
+    LOCALHOST_LINK = str()
+
+    def visit_iati_standard_page(self, admin_browser):
+        """Go to the IATI Stadard page from the admin site."""
+        page_button = admin_browser.find_by_xpath('//a[@href="/en/iati-standard/"]').first
+        page_link = page_button._element.get_property('href')
+        self.LOCALHOST_LINK = page_link[:-15]
+        admin_browser.visit(page_link)
+
+    ORG_STANDARD = {'id': 'org-standard', 'expected_content': 'This section details the IATI Organisation standard.'}
+    ACTIVITY_STANDARD = {'id': 'act-standard', 'expected_content': 'This section details the IATI Activity standard.'}
+    CODELISTS = {'id': 'codelists', 'expected_content': 'The IATI codelists are key to making IATI activity and organisation data from different publishers comparable.'}
+    SCHEMA = {'id': 'schema', 'expected_content': 'The IATI standard consists of a number of schema,'}
+    ORG_IDS = {'id': 'org-ids', 'expected_content': 'Organisational identifiers have two uses in IATI.'}
+    STANDARD_UPGRADE = {'id': 'standard-upgrade', 'expected_content': 'The IATI standard is a living entity that will require improvement over time.'}
+    PREV_VERSIONS = {'id': 'prev-versions', 'expected_content': 'Links to all versions IATI Standard documentation are available below.'}
+    VER_202 = {'id': '202', 'expected_content': 'This is version 2.02 of the IATI Standard.'}
+    VER_201 = {'id': '201', 'expected_content': 'This is version 2.01 of the IATI Standard.'}
+    VER_105 = {'id': '105', 'expected_content': 'This is version 1.05 of the IATI Standard.'}
+    INFO_FOR_DEVS = {'id': 'info-for-devs', 'expected_content': 'This section contains various pieces documentation targetted at developers'}
+    DATASTORE_GUIDE = {'id': 'datastore-guide', 'expected_content': 'The Datastore stores all activity data available on the IATI Registry,'}
+
+    HARDCODED_LINKS = [ORG_STANDARD, ACTIVITY_STANDARD, CODELISTS, SCHEMA, ORG_IDS, STANDARD_UPGRADE, PREV_VERSIONS, VER_202, VER_201, VER_105, INFO_FOR_DEVS, DATASTORE_GUIDE]
+
+    @pytest.mark.parametrize('link', HARDCODED_LINKS)
+    def test_hardcoded_links_work(self, admin_browser, link):
+        """Check that the given link goes to the correct page.
+
+        Note:
+            The weird browser visit after the test assertion is due to a login error occuring during parametrization that wasn't logging out of the admin site and then logging in fresh for the next parametrized test.
+
+        """
+        self.visit_iati_standard_page(admin_browser)
+        admin_browser.find_by_id(link['id']).click()
+        assert admin_browser.is_text_present(link['expected_content'])
+        admin_browser.visit(self.LOCALHOST_LINK)
