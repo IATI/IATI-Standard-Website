8c70aef245a59e49051c3ab5ca5ed2bb813cc9d7
Alex Miller <alex.k.miller@gmail.com>
2018-07-02 13:30:41 -0400

    Docstring

--- iati/home/views.py
+++ iati/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect
 
 
 def reference_redirect(request, *args, **kwargs):
+    """A functional view that accepts any request starting with a version number."""
     base_url = "http://reference.iatistandard.org"
     slug = request.get_full_path()
     redirection_url = base_url + slug
