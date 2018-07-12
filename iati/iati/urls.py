b21c41a7ae5949ae173193f3cbf5d876c8aac42c
Andy Lulham <a.lulham@gmail.com>
2018-07-12 13:13:42 +0200

    Remove clobbered import

--- iati/iati/urls.py
+++ iati/iati/urls.py
@@ -2,7 +2,6 @@
 
 from django.conf import settings
 from django.conf.urls import include, url
-from django.conf.urls.i18n import i18n_patterns  # For internationalization
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
