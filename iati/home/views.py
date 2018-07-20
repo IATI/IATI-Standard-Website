0ee36902f96da020ebe8cee808e5359990760774
Alex Miller <alex.k.miller@gmail.com>
2018-07-20 09:24:28 -0400

    Fix broken merge

--- iati/home/views.py
+++ iati/home/views.py
@@ -3,13 +3,8 @@
 from django.shortcuts import redirect
 
 
-<<<<<<< HEAD
-def reference_redirect(request):
-    """Functional view that accepts any request starting with a reference namespace."""
-=======
 def reference_redirect(request, *args, **kwargs):
     """A functional view that accepts any request starting with a reference namespace."""
->>>>>>> parent of 9d7e6e3... Remove unused arbitrary args
     base_url = "http://reference.iatistandard.org"
     slug = request.get_full_path()
     redirection_url = base_url + slug
