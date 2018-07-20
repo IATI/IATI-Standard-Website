ad0c1a410aaffe2f6748bbbe83100972646d04bb
Alex Miller <alex.k.miller@gmail.com>
2018-07-20 09:28:34 -0400

    Docstring

--- iati/home/views.py
+++ iati/home/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import redirect
 
 
 def reference_redirect(request, *args, **kwargs):
-    """A functional view that accepts any request starting with a reference namespace."""
+    """Functional view that accepts any request starting with a reference namespace.
+
+    NOTE: args and kwargs are not used, but are required to catch all positional arguments.
+    """
     base_url = "http://reference.iatistandard.org"
     slug = request.get_full_path()
     redirection_url = base_url + slug
