d5172e8335e390e22478b1456d6aa9b2dabbf5e0
Alex Miller <alex.k.miller@gmail.com>
2018-08-23 12:35:14 -0400

    A bit wonky, but working

--- iati/using_data/models.py
+++ iati/using_data/models.py
@@ -5,5 +5,3 @@ from about.models import AboutPage
 
 class UsingDataPage(AboutPage):
     """A page model for the Using IATI data page. Inherits all from AboutPage."""
-
-    template = 'about/about_page.html'
