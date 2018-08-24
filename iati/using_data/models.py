f0174c135491dca15922bee627ac6bcc081c1e79
Ocre42 <john.askew@devinit.org>
2018-08-24 10:47:24 +0100

    Switching tools_index side panel to the left side, adding models and tools_page single template

--- iati/using_data/models.py
+++ iati/using_data/models.py
@@ -1,7 +1,22 @@
 """Model definitions for the using_data app."""
 
-from about.models import AboutPage
+from about.models import AboutPage, AboutSubPage
 
 
 class UsingDataPage(AboutPage):
     """A page model for the Using IATI data page. Inherits all from AboutPage."""
+
+    subpage_types = ['using_data.ToolsIndexPage', 'about.AboutSubPage']
+
+
+class ToolsIndexPage(AboutSubPage):
+    """A page model for the Tools & Resources page. Inherits all from AboutSubPage."""
+
+    parent_page_types = ['using_data.UsingDataPage']
+    subpage_types = ['using_data.ToolsPage']
+
+
+class ToolsPage(AboutSubPage):
+    """A page model for single Tools & Resources pages. Inherits all form AboutSubPage."""
+
+    parent_page_types = ['using_data.ToolsIndexPage']
