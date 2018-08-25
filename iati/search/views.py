b96cb926637c322b6665e1a564b3c6d594533cd0
Andy Lulham <a.lulham@gmail.com>
2018-08-25 18:02:35 +0100

    Search each content page type in series

--- iati/search/views.py
+++ iati/search/views.py
@@ -2,20 +2,35 @@
 
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
-
-from wagtail.core.models import Page
 from wagtail.search.models import Query
+from wagtail.core.models import Page
+
+from about.models import AboutPage, AboutSubPage, CaseStudyPage, HistoryPage, PeoplePage
+from contact.models import ContactPage
+from events.models import EventPage
+from guidance_and_support.models import GuidanceAndSupportPage, GuidanceGroupPage, \
+    GuidancePage, KnowledgebasePage
+from news.models import NewsPage
+from home.models import StandardPage
+from iati_standard.models import IATIStandardPage
 
 
 def search(request):
     """Process a user input for a search query and return a page containing results."""
     per_page = 10
+    searchable_models = [
+        AboutPage, AboutSubPage, CaseStudyPage, HistoryPage,
+        PeoplePage, ContactPage, EventPage, GuidanceAndSupportPage,
+        GuidanceGroupPage, GuidancePage, KnowledgebasePage, NewsPage,
+        StandardPage, IATIStandardPage,
+    ]
     search_query = request.GET.get('query', None)
     page = request.GET.get('page', 1)
 
     # Search
     if search_query:
-        search_results = Page.objects.live().search(search_query)
+        search_results = [r for m in searchable_models
+                          for r in m.objects.live().search(search_query)]
         query = Query.get(search_query)
 
         # Record hit
