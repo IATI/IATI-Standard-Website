d100401d6101b0f5109dc59d5a048dbba50c9fbc
Samuele Mattiuzzo <samumatt@gmail.com>
2018-09-25 17:27:02 +0100

    Sitemap in INSTALLED_APPS and urls #226

--- iati/iati/urls.py
+++ iati/iati/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
@@ -54,6 +55,9 @@ urlpatterns += [
 
 
 urlpatterns += i18n_patterns(
+    # Wagtail sitemap
+    url('^sitemap\.xml$', sitemap),
+
     # These URLs will have /<language_code>/ appended to the beginning
     url(r'^search/$', search_views.search, name='search'),
 
