f7eb5cee25207d72ebd9cad9f5f8484344eccd07
Francesca Sargent <francesca.sargent@devinit.org>
2019-02-22 13:56:19 +0000

    Reconfiguring middleware for redirects

--- iati/iati/custom_middleware.py
+++ iati/iati/custom_middleware.py
@@ -1,44 +1,9 @@
 """Middleware for redirecting mixed case urls into lowercase."""
 from django import http
 from django.conf import settings
+from django.utils.functional import cached_property
 
-EXTERNAL_REDIRECTS = [
-    "/101/",
-    "/102/",
-    "/103/",
-    "/104/",
-    "/105/",
-    "/201/",
-    "/202/",
-    "/203/",
-    "/activity-standard/",
-    "/codelists/",
-    "/developer/",
-    "/introduction/",
-    "/namespaces-extensions/",
-    "/organisation-identifiers/",
-    "/organisation-standard/",
-    "/reference/",
-    "/rulesets/",
-    "/schema/",
-    "/upgrades/",
-    "/guidance/datastore/",
-]
-
-
-def check_exceptions(path):
-    """Review exceptions to the lowercase ruling."""
-    if path.startswith(settings.MEDIA_URL):
-        # Make sure it's not a media file
-        return False
-    for external in EXTERNAL_REDIRECTS:
-        if path.startswith(external):
-            return False
-    for code, _ in settings.ACTIVE_LANGUAGES:
-        # Check for all active languages
-        if path.startswith("/" + code + "/documents/"):
-            return False
-    return True
+from wagtail.contrib.redirects.models import Redirect
 
 
 class LowercaseMiddleware:
@@ -53,6 +18,31 @@ class LowercaseMiddleware:
         response = self.get_response(request)
         path = request.get_full_path()
         lower_path = path.lower()
-        if lower_path != path and check_exceptions(path):
+        if self.path_is_exception(path, lower_path):
             return http.HttpResponsePermanentRedirect(lower_path)
         return response
+
+    @cached_property
+    def redirects(self):
+        """Return tuple of original redirect paths if redirect is external."""
+        return tuple(x.old_path for x in Redirect.objects.all() if x.redirect_link)
+
+    @cached_property
+    def exception_values(self):
+        """Return values to except from lowercase ruling."""
+        return (settings.ADMIN_URL, settings.MEDIA_URL, settings.DOCUMENTS_URL)
+
+    def path_is_exception(self, path, lower_path):
+        """Review exceptions to the lowercase ruling."""
+        if lower_path == path:
+            return False
+        if path == '/':
+            return False
+        if path.startswith(self.exception_values):
+            return True
+        if path.startswith(self.redirects):
+            return True
+
+        # documents still fail, we need to investigate why
+
+        return False
