ca3b3de6d1a15ece75a854d73c19c52f749e51a9
Ocre42 <john.askew@devinit.org>
2019-03-06 12:04:39 +0000

    Randomize document factory

--- iati/home/tests/factories.py
+++ iati/home/tests/factories.py
@@ -1,11 +1,15 @@
 import factory
+from factory.faker import faker
 from wagtail.documents.models import Document
 from wagtail.images.models import Image
 
+def random_filename():
+	filegen = faker.Faker()
+	return filegen.random_uppercase_letter() + filegen.file_name()
 
 class DocumentFactory(factory.django.DjangoModelFactory):
 	title = factory.Sequence(lambda n: f"document {n}")
-	file = factory.django.FileField(filename="MiXeDcAsE.txt", data=b'Test document')
+	file = factory.django.FileField(filename=random_filename(), data=b'Test document')
 
 	class Meta:
 		model = Document
