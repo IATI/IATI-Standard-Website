26dc78ff7349533f51c98b0fa07d20c6538bfda9
Francesca Sargent <francesca@fffunction.co>
2019-03-15 09:15:21 +0000

    Adding site hostnames to test

--- iati/home/tests/test_middleware.py
+++ iati/home/tests/test_middleware.py
@@ -2,6 +2,8 @@
 import pytest
 from django.conf import settings
 from factories import DocumentFactory
+from wagtail.core.models import Site, Page
+from about.models import AboutPage
 
 
 @pytest.mark.django_db
@@ -17,11 +19,15 @@ class TestRedirectMiddleware():
     # ])
     def test_redirect_middleware_internal(self, client):
         """Test behavior for internal redirects."""
-        site = client.get('/',follow=True).context.get('request').site
-        print('Root URL', site.root_url)
-        # response = client.get(redirect_mapping[0])
-        # assert response.url == redirect_mapping[1]
-        assert True
+        client_request = client.get('/', follow=True).context.get('request')
+        site = Site.find_for_request(client_request)
+        site.hostname = client_request.get_host()
+        site.save()
+        home = Page.objects.get(id=3)
+        about = AboutPage(title='About')
+        home.add_child(instance=about)
+        response = client.get('/About', follow=True)
+        assert response.status_code == 200
 
     @pytest.mark.parametrize('redirect_mapping', [
         ('/203/codelists/OtherIdentifierType/', 'http://reference.iatistandard.org/203/codelists/OtherIdentifierType/'),
