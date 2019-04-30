0bad9653ad8efcbd3cba3f896903d560dc8d9df6
Francesca Sargent <francesca.sargent@devinit.org>
2019-04-30 10:54:25 +0100

    Amending linting for base settings

--- iati/iati/settings/base.py
+++ iati/iati/settings/base.py
@@ -231,6 +231,6 @@ MODELTRANSLATION_PO_FILE = "iati.po"
 
 ZENDESK_REQUEST_URL = 'https://iati.zendesk.com/api/v2/requests.json'
 
-## Social Media
+# Social Media
 TWITTER_HANDLE = 'IATI_aid'
 YOUTUBE_CHANNEL_URL = 'https://www.youtube.com/channel/UCAVH1gcgJXElsj8ENC-bDQQ'
