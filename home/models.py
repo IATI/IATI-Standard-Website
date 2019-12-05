c43a3f399bbcdc589e2c15ba4396237e723ab5d8
Ocre42 <john.askew@devinit.org>
2019-12-05 11:58:29 +0000

    Explicitly give the static social image

--- home/models.py
+++ home/models.py
@@ -157,7 +157,7 @@ class AbstractBasePage(Page):
         if hasattr(self, 'feed_image'):
             if self.feed_image:
                 return self.feed_image.get_rendition('min-300x300|jpegquality-60').url
-        return static(settings.DEFAULT_SHARE_IMAGE_URL)
+        return static('img/iati-share-social.png')
 
     @property
     def search_display_name(self):
