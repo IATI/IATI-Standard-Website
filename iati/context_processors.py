c7788ce69766532af7b50a0451c8b9ef551872ab
Russell Kirkland <russell@mashandgravy.co.uk>
2020-01-09 11:03:52 +0000

    Amends first lines to imperative mood to comply with pydocstyle

--- iati/context_processors.py
+++ iati/context_processors.py
@@ -2,12 +2,12 @@ from tools.models import ToolsListingPage
 
 
 def has_new_tools_page():
-    """Returns the first new tools page if it exists."""
+    """Return the first new tools page if it exists."""
     return ToolsListingPage.objects.live().first()
 
 
 def globals(request):
-    """Returns a global context dictionary for use by templates."""
+    """Return a global context dictionary for use by templates."""
 
     return {
         'global': {
