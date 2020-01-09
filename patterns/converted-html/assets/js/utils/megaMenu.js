940aad316304dfec3e19221c607f142996670e50
Russell Kirkland <russell@mashandgravy.co.uk>
2020-01-09 16:50:43 +0000

    Fixes to meganav for mobile/desktop views

--- patterns/converted-html/assets/js/utils/megaMenu.js
+++ patterns/converted-html/assets/js/utils/megaMenu.js
@@ -11,40 +11,80 @@ export default function megaMenu (
     hoverClass = 'hover',
     focusClass = 'focus',
     openClass = 'open',
-    minWidth = 1000
+    minWidth = 950
     ) {
 
-    const isMinWidth = window.matchMedia(`(min-width: ${minWidth}px)`).matches;
+    // clone the original nav for mobile and enhanced versions
+    let original_nav = $(selector).clone(true, true);
+    let enhanced_nav = $(selector).clone(true, true);
+    let is_enhanced = false;
 
-    if (isMinWidth) {
+    // add a window event handler
+    $(window).on('resize', e => { onResize()});
 
-        // accessible megamenu
-        $(selector).accessibleMegaMenu({
-            // prefix for generated unique id attributes, which are required
-            // to indicate aria-owns, aria-controls and aria-labelledby
-            uuidPrefix: uuidPrefix,
+    // initialise with a resize call
+    onResize();
 
-            // CSS class used to define the megamenu styling
-            menuClass: menuClass,
+    // on resize, check width and call appropriate function
+    function onResize(e) {
+        const is_min_width = window.matchMedia(`(min-width: ${minWidth}px)`).matches;
 
-            // CSS class for a top-level navigation item in the megamenu
-            topNavItemClass: topNavItemClass,
+        console.log('onResize');
 
-            // CSS class for a megamenu panel
-            panelClass: panelClass,
 
-            // CSS class for a group of items within a megamenu panel
-            panelGroupClass: panelGroupClass,
+        if (is_min_width) {
+            replaceMenuWithEnhanced();
+        }
+        else {
+            replaceMenuWithOriginal();
+        }
+    }
 
-            // CSS class for the hover state
-            hoverClass: hoverClass,
+    // if enhanced, replace curent nav with enhanced version
+    function replaceMenuWithEnhanced() {
+        console.log('replaceMenuWithEnhanced');
+        enhanced_nav = $(selector).first().replaceWith(enhanced_nav);
 
-            // CSS class for the focus state
-            focusClass: focusClass,
+        // initialise if not done so already
+        if (!is_enhanced) {
 
-            // CSS class for the open state
-            openClass: openClass
-        });
+            // accessible megamenu
+            enhanced_nav.accessibleMegaMenu({
+                // prefix for generated unique id attributes, which are required
+                // to indicate aria-owns, aria-controls and aria-labelledby
+                uuidPrefix: uuidPrefix,
 
+                // CSS class used to define the megamenu styling
+                menuClass: menuClass,
+
+                // CSS class for a top-level navigation item in the megamenu
+                topNavItemClass: topNavItemClass,
+
+                // CSS class for a megamenu panel
+                panelClass: panelClass,
+
+                // CSS class for a group of items within a megamenu panel
+                panelGroupClass: panelGroupClass,
+
+                // CSS class for the hover state
+                hoverClass: hoverClass,
+
+                // CSS class for the focus state
+                focusClass: focusClass,
+
+                // CSS class for the open state
+                openClass: openClass
+            });
+
+            // set flag and store for future switching
+            is_enhanced = true;
+            enhanced_nav = $(selector).first();
+        }
+    }
+
+    function replaceMenuWithOriginal() {
+        console.log('replaceMenuWithOriginal');
+        // replace and store for future switching
+        original_nav = $(selector).first().replaceWith(original_nav);
     }
 }
