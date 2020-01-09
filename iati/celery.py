5933759b85b2e6aec7b2f75bc69800226c4a0f26
Alex Miller <alex.k.miller@gmail.com>
2020-01-09 15:44:18 -0500

    Docstrings

--- iati/celery.py
+++ iati/celery.py
@@ -1,18 +1,14 @@
+"""Module to configure Celery app."""
+
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
 
 app = Celery('data_tasks')
-
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
 
 @app.task(bind=True)
 def debug_task(self):
+    """Print debug log for Celery requests."""
     print('Request: {0!r}'.format(self.request))
