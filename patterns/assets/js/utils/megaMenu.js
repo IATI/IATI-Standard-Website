0970c73c87ede766b0510754b5d9c1439ac1f00e
Russell Kirkland <russell@mashandgravy.co.uk>
2020-01-15 10:51:09 +0000

    Fixes meganav to work between mobile and enhanced desktop

--- patterns/assets/js/utils/megaMenu.js
+++ patterns/assets/js/utils/megaMenu.js
@@ -15,9 +15,8 @@ export default function megaMenu (
     ) {
 
     // clone the original nav for mobile and enhanced versions
-    let original_nav = $(selector).clone(true, true);
-    let enhanced_nav = $(selector).clone(true, true);
-    let is_enhanced = false;
+    const original_nav = $(selector).clone(true, true);
+    const enhanced_nav = $(selector).clone(true, true);
 
     // add a window event handler
     $(window).on('resize', e => { onResize()});
@@ -29,9 +28,6 @@ export default function megaMenu (
     function onResize(e) {
         const is_min_width = window.matchMedia(`(min-width: ${minWidth}px)`).matches;
 
-        console.log('onResize');
-
-
         if (is_min_width) {
             replaceMenuWithEnhanced();
         }
@@ -40,51 +36,42 @@ export default function megaMenu (
         }
     }
 
-    // if enhanced, replace curent nav with enhanced version
+    // replace nav with enhanced version
     function replaceMenuWithEnhanced() {
         console.log('replaceMenuWithEnhanced');
-        enhanced_nav = $(selector).first().replaceWith(enhanced_nav);
+        $(selector).first().off().unbind().replaceWith(enhanced_nav);
 
-        // initialise if not done so already
-        if (!is_enhanced) {
+        // accessible megamenu
+        $(selector).first().accessibleMegaMenu({
+            // prefix for generated unique id attributes, which are required
+            // to indicate aria-owns, aria-controls and aria-labelledby
+            uuidPrefix: uuidPrefix,
 
-            // accessible megamenu
-            enhanced_nav.accessibleMegaMenu({
-                // prefix for generated unique id attributes, which are required
-                // to indicate aria-owns, aria-controls and aria-labelledby
-                uuidPrefix: uuidPrefix,
+            // CSS class used to define the megamenu styling
+            menuClass: menuClass,
 
-                // CSS class used to define the megamenu styling
-                menuClass: menuClass,
+            // CSS class for a top-level navigation item in the megamenu
+            topNavItemClass: topNavItemClass,
 
-                // CSS class for a top-level navigation item in the megamenu
-                topNavItemClass: topNavItemClass,
+            // CSS class for a megamenu panel
+            panelClass: panelClass,
 
-                // CSS class for a megamenu panel
-                panelClass: panelClass,
+            // CSS class for a group of items within a megamenu panel
+            panelGroupClass: panelGroupClass,
 
-                // CSS class for a group of items within a megamenu panel
-                panelGroupClass: panelGroupClass,
+            // CSS class for the hover state
+            hoverClass: hoverClass,
 
-                // CSS class for the hover state
-                hoverClass: hoverClass,
+            // CSS class for the focus state
+            focusClass: focusClass,
 
-                // CSS class for the focus state
-                focusClass: focusClass,
-
-                // CSS class for the open state
-                openClass: openClass
-            });
-
-            // set flag and store for future switching
-            is_enhanced = true;
-            enhanced_nav = $(selector).first();
-        }
+            // CSS class for the open state
+            openClass: openClass
+        });
     }
 
     function replaceMenuWithOriginal() {
-        console.log('replaceMenuWithOriginal');
-        // replace and store for future switching
-        original_nav = $(selector).first().replaceWith(original_nav);
+        // replace with original
+        $(selector).first().replaceWith(original_nav);
     }
 }
