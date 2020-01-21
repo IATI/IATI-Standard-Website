029df0390e85be1bcb791515868b30bd03ce38d5
Russell Kirkland <russell@mashandgravy.co.uk>
2020-01-21 10:18:38 +0000

    Add management command cribbed from https://github.com/wagtail/wagtail/issues/4602#issuecomment-523587952 to check for html errors

---
+++ home/management/commands/htmlerrors.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from django.test import Client
+
+from wagtail.core.models import Page
+from wagtail.admin.views.pages import edit
+
+
+class Command(BaseCommand):
+
+    def handle(self, **options):
+        invalid_page_ids = []
+        pages = Page.objects.all()
+        client = Client()
+        # use any existing super user account
+        user = User.objects.get(username='russell')
+        client.force_login(user)
+
+        for page in pages:
+            id = page.id
+            # print statement to assure the script is running
+            print(id)
+            url = f'/cms/pages/{id}/edit/'
+            try:
+                response = client.get(url)
+            except:
+                invalid_pages.append(id)
+
+        print(invalid_page_ids)
