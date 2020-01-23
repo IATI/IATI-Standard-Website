3f84527e03eecd39725214eab0a49a4f782e3c47
Russell Kirkland <russell@mashandgravy.co.uk>
2020-01-23 15:20:57 +0000

    Add htmlerrors test command

---
+++ home/management/commands/htmlerrors.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from django.test import Client
+
+from wagtail.core.models import Page
+from wagtail.admin.views.pages import edit
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        """Add custom command arguments."""
+        parser.add_argument('pks', nargs='+', type=int)
+
+    def handle(self, **options):
+        invalid_page_ids = []
+        invalid_page_errors = []
+        pages = Page.objects.all()
+        client = Client()
+        # use any existing super user account
+        user = User.objects.get(username='russell')
+        client.force_login(user)
+
+        for page in pages:
+            id = page.id
+            if int(id) in options['pks']:
+                # print statement to assure the script is running
+                print(id)
+                url = f'/cms/pages/{id}/edit/'
+                try:
+                    response = client.get(url)
+                except Exception as e:
+                    invalid_page_ids.append(id)
+                    invalid_page_errors.append({'id': id, 'error': e})
+
+        print(invalid_page_ids)
+        print(invalid_page_errors)
