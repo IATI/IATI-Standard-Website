0619e0fff6008cec2c640140a9395c07a940002d
Russell Kirkland <russell@mashandgravy.co.uk>
2020-01-27 17:15:27 +0000

    Initial page list column template

--- navigation/values.py
+++ navigation/values.py
@@ -2,12 +2,23 @@ from wagtail.core.blocks import StructValue
 from navigation.utils import get_localised_field_value, get_default_lang_slug
 
 
-class TransStructValue(StructValue):
+class ModuleStructValue(StructValue):
+    def num_columns(self):
+        return len(self.get('columns'))
 
+
+class TransStructValue(StructValue):
     def description(self):
         return get_localised_field_value(
             self,
             'description',
+            use_get=True,
+        )
+
+    def title(self):
+        return get_localised_field_value(
+            self,
+            'title',
             use_get=True,
         )
 
