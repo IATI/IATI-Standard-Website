cc99682a6479a0bd6f4229fb161fdc8d1ab784ad
Russell Kirkland <russell@mashandgravy.co.uk>
2020-02-03 11:04:52 +0000

    Updates values and type c template

--- navigation/values.py
+++ navigation/values.py
@@ -15,55 +15,58 @@ class ModuleStructValue(StructValue):
 
 
 class ModuleDoubleStructValue(StructValue):
+    def highlight_class(self):
+        highlight_class = ''
+        if len(self.get('columns')) > 2:
+            highlight_class = 'navigation-megamenu__col--small'
+
+        return highlight_class
+
     def has_secondary(self):
         has_secondary_highlight = False
         for item in self.get('columns'):
             if item.block_type == 'secondary_highlight':
                 has_secondary_highlight = True
+
         return has_secondary_highlight
 
     def num_columns(self):
-        return len(self.get('columns'))
+        return max(3, len(self.get('columns')))
 
     def num_columns_max(self):
         max_num = 4
-        num_cols = len(self.get('columns'))
+        num_cols = self.num_columns()
         if num_cols > max_num:
-            return ceil(num_cols / 2)
+            num_cols = ceil(num_cols / 2)
         return num_cols
 
     def num_rows(self):
-        if self.has_secondary() and len(self.get('columns')) > 3:
-            return 2
-        return 1
+        num_rows = 1
+        if self.has_secondary() and self.num_columns() > 3:
+            num_rows = 2
+        return num_rows
 
-    def num_columns_row_1(self):
+    def num_columns_row(self):
         has_secondary = self.has_secondary()
         num_cols = self.num_columns()
-        if has_secondary and num_cols <= 3:
-            return num_cols - 1
-        else:
-            return num_cols
 
-    def num_columns_row_2(self):
-        has_secondary = self.has_secondary()
-        num_cols = self.num_columns()
         if has_secondary:
-            return ceil(num_cols / 2) - 1
-        else:
-            return ceil(num_cols / 2)
+            if num_cols < 4:
+                num_cols = num_cols + 1
+            else:
+                num_cols = num_cols - 1
 
-    def column_container_class_row_1(self):
-        return 'l-%sup' % str(self.num_columns_row_1())
+        num_cols = min(num_cols, 4)
 
-    def column_container_class_row_2(self):
-        return 'l-%sup' % str(self.num_columns_row_2())
+        return num_cols
 
-    def column_class_row_1(self):
-        return 'l-%sup__col' % str(self.num_columns_row_1())
+    def column_container_class(self):
+        print('l-%sup' % str(self.num_columns_row()))
+        return 'l-%sup' % str(self.num_columns_row())
 
-    def column_class_row_2(self):
-        return 'l-%sup__col' % str(self.num_columns_row_2())
+    def column_class(self):
+        print('l-%sup__col' % str(self.num_columns_row()))
+        return 'l-%sup__col' % str(self.num_columns_row())
 
 
 class TransStructValue(StructValue):
