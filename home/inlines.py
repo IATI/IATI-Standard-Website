85008ea14d77daa9c54a9e092b81a6087cfdd4d2
Russell Kirkland <russell@mashandgravy.co.uk>
2020-02-17 15:44:48 +0000

    Attempt to make fields optional at runtime (although think it doesn't work with WMT)

--- home/inlines.py
+++ home/inlines.py
@@ -6,7 +6,35 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 from common.utils import ForeignKeyField, WagtailImageField
 
 
-class GettingStartedItem(models.Model):
+class DynamicOptionalFieldPanel(FieldPanel):
+    '''
+    By default, this field will be required.
+    If the concrete class field should be optional, then add the following to the class definition:
+    [field]_required = False
+    '''
+
+    def on_form_bound(self):
+        name = self.field_name
+        required = getattr(self.model, '%s_required' % name, True)
+        self.form.fields[name].required = required
+        super().on_form_bound()
+
+
+class DynamicOptionalImageChooserPanel(ImageChooserPanel):
+    '''
+    By default, this field will be required.
+    If the concrete class field should be optional, then add the following to the class definition:
+    [field]_required = False
+    '''
+
+    def on_form_bound(self):
+        name = self.field_name
+        required = getattr(self.model, '%s_required' % name, True)
+        self.form.fields[name].required = required
+        super().on_form_bound()
+
+
+class BaseRelatedPageItem(models.Model):
     class Meta:
         abstract = True
 
@@ -14,27 +42,48 @@ class GettingStartedItem(models.Model):
         model='wagtailcore.Page',
         required=True,
         on_delete=models.CASCADE,
+        help_text='Page link for the item'
     )
-    image = WagtailImageField(
-        required=True,
-        help_text='Image for the getting started item'
-    )
+
+
+class BaseRelatedItem(BaseRelatedPageItem):
+    class Meta:
+        abstract = True
+
     title = models.CharField(
         max_length=255,
-        help_text='Title for the getting started item',
+        help_text='Title for the item',
     )
     description = models.CharField(
         max_length=255,
-        help_text='Description for the getting started item',
+        help_text='Description for the item',
     )
+
+
+class BaseRelatedImageItem(BaseRelatedItem):
+    class Meta:
+        abstract = True
+
+    image = WagtailImageField(
+        required=True,
+        help_text='Image for the item'
+    )
+
+
+class GettingStartedItem(BaseRelatedImageItem):
+    class Meta:
+        abstract = True
+
     link_label = models.CharField(
         max_length=255,
-        help_text='Link label for the getting started item',
+        help_text='Link label for the item',
     )
 
 
 class GettingStartedItems(Orderable, GettingStartedItem):
     item = ParentalKey('HomePage', related_name='getting_started_items')
+
+    title_required = True
 
     translation_fields = [
         'title',
@@ -45,7 +94,47 @@ class GettingStartedItems(Orderable, GettingStartedItem):
     panels = [
         PageChooserPanel('page'),
         ImageChooserPanel('image'),
-        FieldPanel('title'),
+        DynamicOptionalFieldPanel('title'),
         FieldPanel('description'),
         FieldPanel('link_label'),
+    ]
+
+
+class IATIInActionFeaturedItem(BaseRelatedImageItem):
+    class Meta:
+        abstract = True
+
+    quote = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text='Optional: quote for the item',
+    )
+    quotee = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text='Optional: the source of the quote',
+    )
+
+
+class IATIInActionFeaturedItems(Orderable, IATIInActionFeaturedItem):
+    item = ParentalKey('HomePage', related_name='iati_in_action_featured_item')
+
+    title_required = False
+    description_required = False
+    image_required = False
+
+    translation_fields = [
+        'title',
+        'description',
+        'quote',
+        'quotee',
+    ]
+
+    panels = [
+        PageChooserPanel('page'),
+        ImageChooserPanel('image'),
+        DynamicOptionalFieldPanel('title'),
+        FieldPanel('description'),
+        FieldPanel('quote'),
+        FieldPanel('quotee'),
     ]
