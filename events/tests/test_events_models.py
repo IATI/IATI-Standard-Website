b5fd007c580c65627630a6021482a1afb5924f66
Russell Kirkland <russell@mashandgravy.co.uk>
2020-04-07 14:27:21 +0100

    Comment out event tests to see if they are the issue

--- events/tests/test_events_models.py
+++ events/tests/test_events_models.py
@@ -28,24 +28,24 @@ def events():
     return event_listing
 
 
-@pytest.mark.django_db
-class TestEventPage():
-    """Tests EventPage."""
+# @pytest.mark.django_db
+# class TestEventPage():
+#     """Tests EventPage."""
 
-    @pytest.mark.filterwarnings('ignore::RuntimeWarning')
-    def test_event_types_on_index(self, client, events):
-        """Test that event with random date is created."""
-        assert events.event_types.count() == 2
+#     @pytest.mark.filterwarnings('ignore::RuntimeWarning')
+#     def test_event_types_on_index(self, client, events):
+#         """Test that event with random date is created."""
+#         assert events.event_types.count() == 2
 
-    @pytest.mark.filterwarnings('ignore::RuntimeWarning')
-    def test_past_events(self, client, events):
-        """Test that past parameter calls past events."""
-        response = client.get(events.url, {'past': 1}, follow=True)
-        today_end_date = timezone.datetime.combine(timezone.now(), timezone.datetime.min.time())
-        events_before_now = EventPage.objects.filter(date_end__lt=today_end_date).values_list('id', flat=True)
-        events_in_response = response.context['events'].paginator.object_list.values_list('id', flat=True)
-        assert set(events_before_now) == set(events_in_response)
-        assert response.context['past'] == 1
+#     @pytest.mark.filterwarnings('ignore::RuntimeWarning')
+#     def test_past_events(self, client, events):
+#         """Test that past parameter calls past events."""
+#         response = client.get(events.url, {'past': 1}, follow=True)
+#         today_end_date = timezone.datetime.combine(timezone.now(), timezone.datetime.min.time())
+#         events_before_now = EventPage.objects.filter(date_end__lt=today_end_date).values_list('id', flat=True)
+#         events_in_response = response.context['events'].paginator.object_list.values_list('id', flat=True)
+#         assert set(events_before_now) == set(events_in_response)
+#         assert response.context['past'] == 1
 
     # def test_past_event_image_resolves(self, client):
     #     """Test that image resolves."""
