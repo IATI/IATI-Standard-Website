3516f8e73891f476bcfdbf00d26da964444c2bc0
Russell Kirkland <russell@mashandgravy.co.uk>
2020-04-08 09:29:12 +0100

    Amend context processor and search page url

--- iati/context_processors.py
+++ iati/context_processors.py
@@ -41,8 +41,8 @@ def globals(request):
             'utility_menu': construct_nav(UtilityMenu.for_site(request.site).utility_menu_links.all(), current_page),
             'useful_links': UsefulLinks.for_site(request.site).useful_links.all(),
             'twitter_handle': settings.TWITTER_HANDLE,
-            'search_page': search_page,
-            'support_page': support_page,
+            'search_page': search_page if search_page else current_page,
+            'support_page': support_page if support_page else current_page,
         },
     }
 
