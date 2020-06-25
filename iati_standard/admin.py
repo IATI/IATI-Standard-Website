4ef2df661cadcac9444a5ecdfa77080d7e99929c
Alex Miller <alex.k.miller@gmail.com>
2020-06-25 14:58:05 -0400

    Reference menu

--- iati_standard/admin.py
+++ iati_standard/admin.py
@@ -1,1 +1,21 @@
 """Module for registering admin models for the iati_standard app."""
+
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register
+)
+from iati_standard.models import ReferenceMenu
+
+
+class ReferenceMenuAdmin(ModelAdmin):
+    """Admin model for news pages."""
+
+    model = ReferenceMenu
+    menu_icon = 'doc-full'
+    menu_order = 145
+    menu_label = 'Standard Reference Navigation'
+    list_display = ('tag', 'menu_type', )
+    search_fields = ('tag', )
+
+
+modeladmin_register(ReferenceMenuAdmin)
