836bd19b377357101e04b3a367618c0fa6f8525d
Alex Miller <alex.k.miller@gmail.com>
2020-07-01 07:40:50 -0400

    Update tests

--- home/tests/test_middleware.py
+++ home/tests/test_middleware.py
@@ -28,8 +28,8 @@ class TestRedirectMiddleware():
         assert response.status_code == 200
 
     @pytest.mark.parametrize('redirect_mapping', [
-        ('/203/codelists/OtherIdentifierType/', 'http://reference.iatistandard.org/203/codelists/OtherIdentifierType/'),
-        ('/schema/downloads/CHANGES.txt', 'http://reference.iatistandard.org/schema/downloads/CHANGES.txt')
+        ('/203/codelists/OtherIdentifierType/', 'https://iatistandard.org/en/iati-standard/203/codelists/OtherIdentifierType/'),
+        ('/schema/downloads/CHANGES.txt', 'https://iatistandard.org/en/iati-standard/203/schema/downloads/CHANGES.txt')
     ])
     def test_redirect_middleware_external(self, client, redirect_mapping):
         """Test behavior for external redirects."""
