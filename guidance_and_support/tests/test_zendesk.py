7d44cd2af3cc38999b1c1b897aa5f7ad4b30f63e
Alex Miller <alex.k.miller@gmail.com>
2020-07-20 16:14:10 -0400

    Parameterize test for spambot

--- guidance_and_support/tests/test_zendesk.py
+++ guidance_and_support/tests/test_zendesk.py
@@ -16,6 +16,7 @@ LEGITIMATE_USER['form'].cleaned_data = {
     'query': 'A very serious matter.',
     'name': 'A legitimate user'
 }
+LEGITIMATE_USER['score'] = 1.0
 LEGITIMATE_USER['expected_output'] = {
     'request': {
         'requester': {
@@ -28,9 +29,22 @@ LEGITIMATE_USER['expected_output'] = {
         'subject': 'Automated request from A legitimate user'
     }
 }
+SPAM_BOT = {}
+SPAM_BOT['request'] = HttpRequest()
+SPAM_BOT['request'].META['SERVER_NAME'] = "iatistandard.org"
+SPAM_BOT['request'].path = "/en/a-test-path"
+SPAM_BOT['form'] = forms.Form()
+SPAM_BOT['form'].cleaned_data = {
+    'phone': '',
+    'email': 'test@user.com',
+    'query': 'A very unserious matter.',
+    'name': 'A not legitimate user'
+}
+SPAM_BOT['score'] = 0.0
+SPAM_BOT['expected_output'] = False
 
 
-@pytest.mark.parametrize("user", [LEGITIMATE_USER])
+@pytest.mark.parametrize("user", [LEGITIMATE_USER, SPAM_BOT])
 def test_generate_ticket(user):
     """Test a ticket from a valid user and a spam bot."""
     ticket = generate_ticket(user['request'], user['form'])
