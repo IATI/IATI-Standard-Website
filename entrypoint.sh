707974408747a4fd1297f6bdc2a2b6fbf06a4ef4
Alex Miller <alex.k.miller@gmail.com>
2021-01-13 15:55:55 -0500

    Working single container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e
 
-# >&2 echo "$DATABASE_URL"
-# until psql $DATABASE_URL -c '\l'; do
-#   >&2 echo "Postgres is unavailable - sleeping"
-#   sleep 1
-# done
+>&2 echo "$DATABASE_URL"
+until psql $DATABASE_URL -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
 
 
-# python manage.py collectstatic --noinput
-# python manage.py migrate
-# python manage.py compilemessages
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+python manage.py compilemessages
 
 rc-service elasticsearch.service start
 rc-service celeryd start
+rc-service rabbitmq-server.service start
 
 exec "$@"
