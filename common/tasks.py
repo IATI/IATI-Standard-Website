07367f401feec3ac129a3f2e65be02cb9b0f6907
Alex Miller <alex.k.miller@gmail.com>
2021-01-15 14:59:41 -0500

    Lint and edit workflow to utilize compose

--- common/tasks.py
+++ common/tasks.py
@@ -4,5 +4,5 @@ from celery import shared_task
 
 @shared_task()
 def multiplication_test(x, y):
-    """Simple test of mulitiplication using Celery."""
+    """Test multiplication using Celery."""
     return x * y
