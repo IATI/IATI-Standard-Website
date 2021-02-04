745fa6126b011816fbe7d8ec889f51c7f0aa0ef6
Alex Miller <alex.k.miller@gmail.com>
2021-02-04 16:43:51 -0500

    Lint

--- iati/settings/base.py
+++ iati/settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import sys
 import dj_database_url
 
 # Mark language names as translation strings
