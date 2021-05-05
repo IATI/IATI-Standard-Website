8fe75d5e2379586ae84c3158cf0cd833b83cb8b5
akmiller01 <alex.k.miller@gmail.com>
2021-05-05 12:37:29 -0400

    Fix ref download location bug

--- iati/settings/base.py
+++ iati/settings/base.py
@@ -580,6 +580,7 @@ GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
 
 if AZURE_ACCOUNT_NAME:
+    REFERENCE_DOWNLOAD_ROOT = os.path.join('/reference_downloads')
     AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
     AZURE_CONTAINER = os.getenv('AZURE_CONTAINER')
     DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
