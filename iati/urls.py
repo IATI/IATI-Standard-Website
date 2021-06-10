e67daa498790a4cb43b18419323e10f6f92e1ebc
akmiller01 <alex.k.miller@gmail.com>
2021-06-10 11:57:39 -0400

    url deprecation

--- iati/urls.py
+++ iati/urls.py
@@ -1,7 +1,8 @@
 """Django URL settings. Defines the routing of user requests."""
 
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import re_path
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
@@ -15,21 +16,21 @@ from .activate_languages import i18n_patterns  # For internationalization
 
 
 urlpatterns = [  # pylint: disable=invalid-name
-    url(r'^django-{}/'.format(settings.ADMIN_SLUG), admin.site.urls),
-    url(r'^{}/'.format(settings.ADMIN_SLUG), include(wagtailadmin_urls)),
-    url(r'^{}/'.format(settings.DOCUMENTS_SLUG), include(wagtaildocs_urls)),
+    re_path(r'^django-{}/'.format(settings.ADMIN_SLUG), admin.site.urls),
+    re_path(r'^{}/'.format(settings.ADMIN_SLUG), include(wagtailadmin_urls)),
+    re_path(r'^{}/'.format(settings.DOCUMENTS_SLUG), include(wagtaildocs_urls)),
 ]
 
 
 urlpatterns += i18n_patterns(
     # Wagtail sitemap
-    url(r'^sitemap\.xml$', sitemap),
+    re_path(r'^sitemap\.xml$', sitemap),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    url(r'', include(wagtail_urls)),
-    url(r'^([^/]*)/(\d*)/([^/]*)/[^/]*$', serve, name='wagtailimages_serve'),
+    re_path(r'', include(wagtail_urls)),
+    re_path(r'^([^/]*)/(\d*)/([^/]*)/[^/]*$', serve, name='wagtailimages_serve'),
 
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
