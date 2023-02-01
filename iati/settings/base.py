5437f752c5b14a951594431828e3291111635695
ThatcherK <kaliithatmau@gmail.com>
2023-02-01 12:10:53 +0300

    Remove old emails from admin list

--- iati/settings/base.py
+++ iati/settings/base.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 ADMINS = (
     ('Alex Miller', 'alex.miller@devinit.org'),
-    ('Nik Osvalds', 'nik.osvalds@devinit.org'),
-    ('Alex Lydiate', 'alexl@devinit.org'),
+    ('Chris Wakibi', 'chrisw@devinit.org'),
     ('IATI Code', 'code@iatistandard.org'),
 )
 
