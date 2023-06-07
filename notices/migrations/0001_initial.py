4e1db9c2faaef1904b206d040ba5391d182b9ee7
David Ebukali <debukali@gmail.com>
2023-06-07 16:52:52 +0300

    Migrations to update module paths for wagtail v3.0

--- notices/migrations/0001_initial.py
+++ notices/migrations/0001_initial.py
@@ -2,7 +2,7 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
-import wagtail.core.fields
+import wagtail.fields
 
 
 class Migration(migrations.Migration):
@@ -19,11 +19,11 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('notice_type', models.CharField(choices=[('notice', 'Notice (blue)'), ('alert', 'Alert (yellow)'), ('warning', 'Warning (red)')], default='notice', max_length=255)),
-                ('content', wagtail.core.fields.RichTextField(help_text='Content for the notice')),
-                ('content_en', wagtail.core.fields.RichTextField(help_text='Content for the notice', null=True)),
-                ('content_fr', wagtail.core.fields.RichTextField(help_text='Content for the notice', null=True)),
-                ('content_es', wagtail.core.fields.RichTextField(help_text='Content for the notice', null=True)),
-                ('content_pt', wagtail.core.fields.RichTextField(help_text='Content for the notice', null=True)),
+                ('content', wagtail.fields.RichTextField(help_text='Content for the notice')),
+                ('content_en', wagtail.fields.RichTextField(help_text='Content for the notice', null=True)),
+                ('content_fr', wagtail.fields.RichTextField(help_text='Content for the notice', null=True)),
+                ('content_es', wagtail.fields.RichTextField(help_text='Content for the notice', null=True)),
+                ('content_pt', wagtail.fields.RichTextField(help_text='Content for the notice', null=True)),
             ],
             options={
                 'abstract': False,
@@ -34,11 +34,11 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('notice_type', models.CharField(choices=[('notice', 'Notice (blue)'), ('alert', 'Alert (yellow)'), ('warning', 'Warning (red)')], default='notice', max_length=255)),
-                ('content', wagtail.core.fields.RichTextField(help_text='Content for the notice')),
-                ('content_en', wagtail.core.fields.RichTextField(help_text='Content for the notice', null=True)),
-                ('content_fr', wagtail.core.fields.RichTextField(help_text='Content for the notice', null=True)),
-                ('content_es', wagtail.core.fields.RichTextField(help_text='Content for the notice', null=True)),
-                ('content_pt', wagtail.core.fields.RichTextField(help_text='Content for the notice', null=True)),
+                ('content', wagtail.fields.RichTextField(help_text='Content for the notice')),
+                ('content_en', wagtail.fields.RichTextField(help_text='Content for the notice', null=True)),
+                ('content_fr', wagtail.fields.RichTextField(help_text='Content for the notice', null=True)),
+                ('content_es', wagtail.fields.RichTextField(help_text='Content for the notice', null=True)),
+                ('content_pt', wagtail.fields.RichTextField(help_text='Content for the notice', null=True)),
                 ('allow_dismiss', models.BooleanField(blank=True, default=False)),
                 ('display_location', models.CharField(choices=[('all', 'All pages'), ('selected_page', 'Selected page only'), ('selected_page_and_children', 'Selected page and child pages'), ('children_only', 'Children of selected page')], default='selected_page', max_length=255)),
                 ('page', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='+', to='wagtailcore.Page')),
